e2e7daa00eb57c3b393ac7e1f65ca47772c2a2de
KingLord147 <rajprabhu.147@gmail.com>
2025-10-21 00:19:15 +0530

    View Profile Page to display

--- react-day4-profile/src/pages/ViewProfile.jsx
+++ react-day4-profile/src/pages/ViewProfile.jsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { useParams, Link } from "react-router-dom";
 
-import { useParams } from "react-router-dom";
-
-export default function ViewProfile() {
+export default function ViewProfile({ profiles }) {
   const { id } = useParams();
+  const profile = profiles.find((p) => p.id === Number(id));
+
+  if (!profile) {
+    return (
+      <div>
+        <p>Profile not found</p>
+        <Link to="profiles">Back to list</Link>
+      </div>
+    );
+  }
   return (
     <div>
-      <h2>View Profile #{id}</h2>
+      <h2>{profile.name}</h2>
+      <p>Email: {profile.email}</p>
+      <Link to="/profiles">Back to list</Link>
     </div>
   );
 }
