223b481a4655544f63ac2238940d29f94b904fc8
KingLord147 <rajprabhu.147@gmail.com>
2025-10-21 00:13:30 +0530

    Show Profiles

--- react-day4-profile/src/pages/Profiles.jsx
+++ react-day4-profile/src/pages/Profiles.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default function Profiles() {
+export default function Profiles({ profiles }) {
   return (
     <div>
       <h2>Profiles</h2>
-      <p>List of profiles will appear here.</p>
+      {profiles.length === 0 ? (
+        <p>No profiles yet.</p>
+      ) : (
+        <ul>
+          {profiles.map((p) => (
+            <li key={p.id}>
+              <Link to={`/profiles/${p.id}`}>{p.name}</Link> -{p.email}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
