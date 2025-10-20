09df1b382791b6a28e07d559408902d0949ee356
KingLord147 <rajprabhu.147@gmail.com>
2025-10-20 22:35:02 +0530

    folder structring, routing all pages, Adding State Management

--- react-day4-profile/src/App.jsx
+++ react-day4-profile/src/App.jsx
@@ -1,8 +1,31 @@
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { useState } from "react";
+import Home from "./pages/Home";
+import Profiles from "./pages/Profiles";
+import AddProfile from "./pages/AddProfile";
+import ViewProfile from "./pages/ViewProfile";
+
 export default function App() {
+  const [profiles, setProfiles] = useState([]);
   return (
-    <div>
-      <h1>Profile Manager App</h1>
-      <p>Welcome! Navigate using the menu above.</p>
+    <div style={{ padding: 20 }}>
+      <nav style={{ display: "flex", gap: 20 }}>
+        <Link to="/">Home</Link>
+        <Link to="/profiles">Profiles</Link>
+        <Link to="/add">Add Profile</Link>
+      </nav>
+      <hr />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profiles" element={<Profiles profiles={profiles} />} />
+        <Route path="/add" element={<AddProfile setProfiles={setProfiles} />} />
+        <Route
+          path="/profiles/:id"
+          element={<ViewProfile profiles={profiles} />}
+        />
+      </Routes>
     </div>
   );
 }
