ec2beb046415f6bd34bb0e87d932d0c6764fdc6f
KingLord147 <rajprabhu.147@gmail.com>
2025-10-27 22:01:37 +0530

    creating custom hooks for state and Api

--- react-day5/src/hooks/useTickets.js
+++ react-day5/src/hooks/useTickets.js
@@ -1,11 +1,17 @@
+// useQuery - to fetch/read data and cache it
+//useMutation - to perform CRUD like POST/PATCH/DELETE
+//useQueryClient - gives acess to QueryClient instance so you read/write/invalidate
+
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 /**
  * Adapter: get posts from jsonplaceholder and convert into ticket model:
  * { id, title, description, priority, status, assignee }
  * We simulate priority/status/assignee locally.
  */
+// url to fet api data
 const API_URL = "https://jsonplaceholder.typicode.com/posts?_limit=8";
 
+//
 const randomForm = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
 export const fetchTickets = async () => {
@@ -59,4 +65,29 @@ export const deleteTicketApi = async (id) => {
   return id;
 };
 
-//
+//React Query hooks
+
+export const useTicketQuery = () => {
+  return useQuery(["tickets", fetchTickets, { staleTime: 1000 * 60 }]);
+};
+export const useAddTicket = () => {
+  const qc = useQueryClient();
+  return useMutation(addTicketApi, {
+    //optimistic update
+    onMutate: async (newTicket) => {
+      await qc.cancelQueries(["tickets"]);
+      const previous = qc.getQueryData(["tickets"]) || [];
+      qc.setQueryData(["tickets"], (old = []) => [{ ...newTicket }, ...old]);
+      return { previous };
+    },
+    onError: (err, newTicket, context) => {
+      qc.setQueryData(["tickets"], context.previous);
+    },
+    onSettled: () => qc.invalidateQueries(["tickets"]),
+  });
+};
+
+export const useUpdateTicket = () => {
+  const qc = useQueryClient();
+  return useMutation();
+};
