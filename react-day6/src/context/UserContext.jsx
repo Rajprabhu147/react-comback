1980391d3f744607a9e4f8fdb17ff2cadfb23648
KingLord147 <rajprabhu.147@gmail.com>
2025-11-06 00:33:55 +0530

    Step 7 — Create User Context

---
+++ react-day6/src/context/UserContext.jsx
@@ -0,0 +1,41 @@
+/* eslint-disable react-refresh/only-export-components */
+import { createContext, useContext, useEffect, useState } from "react";
+import { supabase } from "../lib/supabaseClient";
+
+const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const session = supabase.auth.getSession();
+    session.then(({ data }) => setUser(data.session?.user || null));
+
+    const { data: listener } = supabase.auth.onAuthStateChange(
+      (_event, session) => {
+        setUser(session?.user || null);
+      }
+    );
+
+    return () => listener.subscription.unsubscribe();
+  }, []);
+
+  const login = async (email, password) =>
+    await supabase.auth.signInWithPassword({ email, password });
+
+  const signup = async (email, password) =>
+    await supabase.auth.signUp({ email, password });
+
+  const logout = async () => {
+    await supabase.auth.signOut();
+    setUser(null);
+  };
+
+  return (
+    <UserContext.Provider value={{ user, login, signup, logout }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => useContext(UserContext);
