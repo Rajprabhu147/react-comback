36a90570a0bec1045e53837fc573007fa131eeef
KingLord147 <rajprabhu.147@gmail.com>
2025-12-12 11:25:34 +0530

    Index.css fix as it overrides theme.css

--- react-day7/travel-dashboard/src/main.jsx
+++ react-day7/travel-dashboard/src/main.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/theme.css"; // And this
 import "./styles/animations.css"; // Add this
-import App from "./App.jsx";
 import "./index.css";
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
